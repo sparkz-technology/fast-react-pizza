4842179f39e7fd486c4f3c0e5b191046ee7a81a9
sutharsan sparkz <62829664+sparkz-technology@users.noreply.github.com>
2023-07-14 23:16:19 +0530

    init

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import eslintPlugin from "vite-plugin-eslint";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), eslintPlugin()],
+});
